6a1cb35f14d874d202bd76986a9d43ec4a1c4027
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2023-04-09 13:55:53 -0400

    added country interactivity, added helper method for debugging circular js structures, created a supercontinent

--- api/leaflet/leaflet.js
+++ api/leaflet/leaflet.js
@@ -1,3 +1,36 @@
+// all this is just for debugging purposes, can safely ignore - since e is a circular structure, we need a custom stringifier to avoid infinite recursion
+JSON.safeStringify = (obj, indent = 2) => {
+    let cache = [];
+    const retVal = JSON.stringify(
+    obj,
+    (key, value) =>
+        typeof value === "object" && value !== null
+        ? cache.includes(value)
+            ? undefined // Duplicate reference found, discard key
+            : cache.push(value) && value // Store value in our collection
+        : value,
+    indent
+    );
+    cache = null;
+    return retVal;
+};
+
+// creates one massive geoJson file of the world
+var theWorld = {
+    "type":"FeatureCollection",
+    "features": [],
+    "style":{
+        "stroke":"#000000",
+        "fill":"#3498DB",
+        "stroke-width":1,
+        "fill-opacity":0.8
+    }
+}
+
+theWorld.features = theWorld.features.concat(north_america_data.features);
+theWorld.features = theWorld.features.concat(south_america_data.features);
+
+// initializes the empty map with no controls
 console.log("joojthewarrior");
 var map = L.map('map', {
     center: [51.505, -0.09],
@@ -15,7 +48,87 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 map.setView([23.276495031663973, 12.121257773548779], 2.53);
 
-L.geoJson(countriesData).addTo(map);
+//simple robin karp hash, uses the name of the country to randomize a color for it
+function colorHash(name){
+    var base = 233, mdl = 1000000007, hash = 0, place = 1, hex = 16777215 + 1;
+    for (i = 0; i < name.length; i++){
+        if (name.charAt(i) == ' ') continue;
+        hash += (place * (name.charAt(i).charCodeAt()) % mdl);
+        hash %= mdl;
+        place = (place * base) % mdl;
+        //console.log(`hash = ${hash}, base = ${base}, char = ${name.charAt(i).charCodeAt()}`);
+    }
+    hash %= hex;
+    //console.log(name + " " + hash.toString(16).padStart(6, '0'));
+    return "#" + hash.toString(16).padStart(6, '0');
+}
+
+// styles the inputted "feature" (the feature is basically a country)
+function style(feature){
+    return {
+        fillColor: colorHash(feature.properties.name),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.5
+    };
+}
+
+// darkens each of the three colors (RGB) 
+function darken(col){
+    var R = col.substring(0, 2), G = col.substring(2, 4), B = col.substring(4, 6);
+    // adds 22 to each pair in the hex - equivalent to adding 34 in decimal
+    R = Math.min(255, parseInt(R, 16) + 34);
+    G = Math.min(255, parseInt(G, 16) + 34);
+    B = Math.min(255, parseInt(B, 16) + 34);
+    //console.log(R.toString(16) + G.toString(16) + B.toString(16));
+    return "#" + R.toString(16) + G.toString(16) + B.toString(16);
+}
+
+// what happens when you hover over this country
+function highlightFeature(e){
+    var country = e.target;
+    console.log(country.feature.properties.name);
+
+    country.setStyle({
+        weight: 1,
+        fillColor: darken(country.options.fillColor.substring(1)),
+        dashArray: '',
+        fillOpacity: 1
+    });
+
+    country.bringToFront();
+}
+
+// simply resets the style back to normal
+function resetHighlight(e){
+    var country = e.target;
+
+    country.setStyle({
+        fillColor: colorHash(country.feature.properties.name),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    });
+}
+
+// describes the interactivity of each country
+function onEachFeature(feature, layer){
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight
+    });
+}
+
+//console.log(JSON.safeStringify(north_america_data));
+
+L.geoJson(theWorld, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
 
 /* example of how to add markers / polygons / lines to the map
 var marker = L.marker([51.5, -0.09]);
