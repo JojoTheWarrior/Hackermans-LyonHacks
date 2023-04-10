adcb5f36bc8be9fedf5be52878935a4965649da1
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2023-04-09 22:09:05 -0400

    changed country_genres to a json file

---
+++ api/spotify/get_similar_songs/similar.js
@@ -0,0 +1,13 @@
+function findSimilarGenres(rootGenre){
+    var affected_countries = [];
+
+    // loops through all countries and the genres they have
+    for (country in all_genres){
+        hasGenre = false;
+
+        for (genre in all_genres[country]){
+            // checking if this genre is related to the rootGenre
+             
+        }
+    }
+}
