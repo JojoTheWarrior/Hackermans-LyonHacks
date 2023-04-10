2f23c4314fc26bf5550c24f70996c433ee8a0b16
Nirvan Rabbani <nir1r7@gmail.com>
2023-04-10 01:31:22 -0400

    brah

--- api/spotify/get_user_profile/src/script.ts
+++ api/spotify/get_user_profile/src/script.ts
@@ -4,9 +4,37 @@ import { redirectToAuthCodeFlow, getAccessToken } from "./authCodeWithPkce";
 const clientId = "6df7668416944a8eb6ad0318fd79f76f";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
+let token: string;
 
 if (!code) {
     redirectToAuthCodeFlow(clientId);
 } else {
     const accessToken = await getAccessToken(clientId, code);
+    token = accessToken;
+    const sidebar = await getTopTracks();
+
+    console.log(sidebar);
+
 }
+
+
+async function fetchWebApi(endpoint: string, method: string, body?: JSON) {
+    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method,
+      body:JSON.stringify(body)
+    });
+
+    console.log(res);
+
+    return await res.json();
+  }
+  
+  async function getTopTracks(){
+    // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+    return (await fetchWebApi(
+      'v1/me/top/tracks?time_range=short_term&limit=4', 'GET'
+    )).items;
+  }
